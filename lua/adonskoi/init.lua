fb8c2b4ded66fd8777a2cd6b296250ac311d2df5
adonskoi <donskoy.alexander@gmail.com>
2024-08-17 12:04:57 +0200

    enter

--- lua/adonskoi/init.lua
+++ lua/adonskoi/init.lua
@@ -1,3 +1,25 @@
 require("adonskoi.remap")
 require("adonskoi.set")
 
+
+-- don't know what the fuck is that
+-- this solve problem when I hit CR I'm went to some file. Don't know why
+--
+_G.log_command = function(command)
+    local file = io.open("/tmp/nvim_command_log.txt", "a")
+    if file then
+        file:write(command .. "\n")
+        file:close()
+    end
+end
+
+-- Set up an autocmd to log commands on Enter
+vim.api.nvim_exec([[
+  augroup LogCommands
+    autocmd!
+    autocmd CmdlineLeave :lua log_command(vim.fn.getcmdline())
+  augroup END
+]], false)
+
+-- Log Enter key press in normal mode without overriding its behavior
+vim.api.nvim_set_keymap('n', '<CR>', ':lua log_command("Enter pressed in normal mode")<CR><CR>', { noremap = true, silent = true })
